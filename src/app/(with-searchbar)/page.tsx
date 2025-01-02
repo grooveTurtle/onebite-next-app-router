d9c3e5379a321d5d7275cfd263a4e2e77e2ed7df
grooveT <grooveturtle29@gmail.com>
2025-01-02 17:54:47 +0900

    3.5) 리액트 서버 컴포넌트 주의사항

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,15 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-component";
 
+// Server Component!
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
