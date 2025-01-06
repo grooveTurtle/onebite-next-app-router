76dfb3d4c40becd0dc199d6ce593e0d94df90a7a
grooveT <grooveturtle29@gmail.com>
2025-01-06 11:42:45 +0900

    5.3) 풀 라우트 캐시

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,5 +1,15 @@
 import { BookData } from "@/types";
 import style from "./page.module.css";
+import { notFound } from "next/navigation";
+
+// generateStaticParams에서 return 한 값 외의 dynamic하게 처리하는 여부
+// false 일시, 1,2,3이외의 페이지는 404로 처리함.
+// export const dynamicParams = false;
+
+// page router의 getStaticPaths와 유사한 기능
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -10,6 +20,11 @@ export default async function Page({
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${p.id}`
   );
+  if (!response.ok) {
+    if (response.status == 404) {
+      notFound();
+    }
+  }
   const book: BookData = await response.json();
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
     book;
